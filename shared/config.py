235088c9377bd21b9c064e69d44cd70eafb79597
AhmedFalahQ <ahmed.falah.alqahtani@gmail.com>
2026-03-02 03:28:40 +0300

    Minor update and fix bugs

--- shared/config.py
+++ shared/config.py
@@ -1,6 +1,6 @@
 """
 config file that contains model IDs and other settings
-its purpose to reduce the amount of changing the code whenever decided to switch models
+its purpose is to eliminate any code changes in Lambdas whenever decided to switch models
 """
 
 import os
@@ -8,16 +8,16 @@ import os
 # AWS Bedrock models IDs
 MODELS={
     "job_analyzer": os.environ.get(
-        "JOB_ANALYZER_MODEL","amazon.nova-2-lite-v1:0"
+        "JOB_ANALYZER_MODEL","us.amazon.nova-2-lite-v1:0"
     ),
     "resume_tailor": os.environ.get(
-        "RESUME_TAILOR_MODEL","anthropic.claude-haiku-4-5-20251001-v1:0"
+        "RESUME_TAILOR_MODEL","global.anthropic.claude-haiku-4-5-20251001-v1:0"
     ),
     "coverletter_generator":os.environ.get(
-        "COVERLETTER_MODEL","anthropic.claude-haiku-4-5-20251001-v1:0"
+        "COVERLETTER_MODEL","global.anthropic.claude-haiku-4-5-20251001-v1:0"
     ),
     "interview_prep":os.environ.get(
-        "INTERVIEW_MODEL","amazon.nova-2-lite-v1:0"
+        "INTERVIEW_MODEL","us.amazon.nova-2-lite-v1:0"
     )
 }
 
@@ -44,5 +44,9 @@ COST_PER_1K_TOKENS={
     "anthropic.claude-haiku-4-5-20251001-v1:0":{
         "input":0.001, # 1 per 1M
         "output":0.005 # 5 per 1M
+    },
+    "meta.llama3-1-70b-instruct-v1:0": {
+        "input": 0.00099,   # $0.99 per 1M tokens
+        "output": 0.00099   # $0.99 per 1M tokens
     }
 }
